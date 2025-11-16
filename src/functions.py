a225e032f4844600b980faa10e0a2fa6d9c42347
SidewaysPhoenix <madams2550@gmail.com>
2025-11-15 19:42:30 -0600

    added image and link extraction

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode, TextType
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -23,3 +24,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 new_list.append(new_delimeter_node)
     return new_list
 
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,5 +1,5 @@
-from functions import split_nodes_delimiter
 import unittest
+from functions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TextType
 
 class TestFunctions(unittest.TestCase):
@@ -15,4 +15,14 @@ class TestFunctions(unittest.TestCase):
         node = TextNode("This is text with a _italic_ word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.ITALIC)
     
-    
+    def test_extract_mkdwn_links(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_mkdwn_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
