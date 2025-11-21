e7f456368355ec39135efdacd6837a226bc2bb42
SidewaysPhoenix <madams2550@gmail.com>
2025-11-21 10:59:30 -0600

    testing directory management

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from textnode import TextNode, TextType
 from functions import text_to_textnodes
+import os
+import shutil
 
 
 def main():
-    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    print(text_to_textnodes(text))
+    source_directory = "/home/madam/workspace/github.com/bootdev/Static-Site-Generator/static"
+    destination_directory = "/home/madam/workspace/github.com/bootdev/Static-Site-Generator/public"
+
+
+    print(shutil.rmtree(destination_directory))
 
 main()
+
+
+
