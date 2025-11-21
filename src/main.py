ca673af67cfef9acc089dd805bba3d76402597b3
SidewaysPhoenix <madams2550@gmail.com>
2025-11-21 12:40:00 -0600

    added static copy to main.py need to add logging

--- src/main.py
+++ src/main.py
@@ -5,13 +5,31 @@ import shutil
 
 
 def main():
+
     source_directory = "/home/madam/workspace/github.com/bootdev/Static-Site-Generator/static"
     destination_directory = "/home/madam/workspace/github.com/bootdev/Static-Site-Generator/public"
+    #clear destination
+    shutil.rmtree(destination_directory)
+    #remake destination
+    os.mkdir(destination_directory)
+    def copy_static(source_directory, destination_directory):
+        source_list = os.listdir(source_directory)
+        for item in source_list:
+            source_item_path = os.path.join(source_directory, item)
+            destination_item_path = os.path.join(destination_directory, item)
+            #if item is a file that does not exist in destination, copy it
+            if os.path.isfile(source_item_path):
+                shutil.copy(source_item_path, destination_directory)
+            #if item is a directory that does not exist in destination, copy it
+            elif os.path.isdir(source_item_path):
+                os.mkdir(destination_item_path)
+                copy_static(source_item_path, destination_item_path)
 
-
-    print(shutil.rmtree(destination_directory))
-
+    copy_static(source_directory,destination_directory)
 main()
 
 
 
+#list directories,files in source
+#if files are in source directory copy them, if they do not exist in destination_directory yet
+#then if first directory in source does not exist make it and then set as current source to go into, otherwise skip and go to next directory to do the same
